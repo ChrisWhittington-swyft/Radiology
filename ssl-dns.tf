4df8d06461063be2aaf9f63a878df6884ca47fa3
Chris <chris.whittington@swyft.host>
2025-11-20 17:04:07 -0500

    Added ssl-dns-nlb-records.tf.disabled

--- ssl-dns.tf
+++ ssl-dns.tf
@@ -4,17 +4,6 @@ data "aws_route53_zone" "vytalmed" {
   private_zone = false
 }
 
-# Look up the NLB created by the ingress controller for each environment
-data "aws_lb" "ingress_nlb" {
-  for_each = toset(local.enabled_environments)
-
-  name = "${lower(local.effective_tenant)}-${each.key}-ing"
-
-  depends_on = [
-    aws_ssm_association.bootstrap_ingress_now
-  ]
-}
-
 #############################################
 # Wildcard ACM cert in workload account/region
 #############################################
@@ -55,35 +44,6 @@ resource "aws_route53_record" "wildcard_validation" {
 resource "aws_acm_certificate_validation" "wildcard" {
   certificate_arn         = aws_acm_certificate.wildcard.arn
   validation_record_fqdns = [for r in aws_route53_record.wildcard_validation : r.fqdn]
-}
-
-
-# ArgoCD host → Primary env NLB (shared ArgoCD for all envs)
-resource "aws_route53_record" "argocd" {
-  provider = aws.dns
-  zone_id  = data.aws_route53_zone.vytalmed.zone_id
-  name     = local.argocd_host         # e.g., argocd.vytalmed.app
-  type     = "CNAME"
-  ttl      = 60
-  records  = [data.aws_lb.ingress_nlb[local.primary_env].dns_name]
-  allow_overwrite = true
-
-  depends_on = [aws_ssm_association.argocd_ingress_now]
-}
-
-# Per-environment app subdomain → NLB
-resource "aws_route53_record" "app_subdomain" {
-  for_each = toset(local.enabled_environments)
-
-  provider = aws.dns
-  zone_id  = data.aws_route53_zone.vytalmed.zone_id
-  name     = local.environments[each.key].app_subdomain         # e.g., ria.vytalmed.app, ria-dev.vytalmed.app
-  type     = "CNAME"
-  ttl      = 60
-  records  = [data.aws_lb.ingress_nlb[each.key].dns_name]
-  allow_overwrite = true
-
-  depends_on = [aws_ssm_association.backend_secret_now]
 }
 
 # Windows Bastion DNS record → Elastic IP
